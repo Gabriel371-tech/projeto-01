2786d8641eaff2461e9f045a23af9bf1431fc117
Gabriel <gabriel.lopes176@etec.sp.gov.br>
2025-09-09 00:23:30 -0300

    modificado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import Newsletter from "../src/components/Newsleeter";
+import Analytics from "./components/Analytics";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Analytics from "./components/Analytics";
 import './index.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
         <Hero />
         <Analytics />
+        <Newsletter />
     </div>
   );
 }
---
+++ src/components/Newsleeter.jsx
@@ -0,0 +1,14 @@
+
+const Newsletter = () => {
+    return (
+        <div className='w-full py-16 text-white'>
+            <div className='max-w-[1240px] mx=auto grid lg:grid-cols-3'>
+                <div className="md:text-5xl sm:text-4xl text-xl">
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Newsletter;
